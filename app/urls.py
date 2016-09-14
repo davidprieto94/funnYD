84522fbe05824d883ecbc87a61063d6e38e790f1
n4itsric <anticris9303@gmail.com>
2016-09-14 10:58:32 -0500

    fix

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 import app.views
 
 urlpatterns = [
-    url(r'^$', app.views.inicio),
+    url(r'^$', app.views.inicio, name='inicio'),
     url(r'^plataforma/$', app.views.plataforma),
     url(r'^cambiocontrasenia/$', app.views.cambiocontrasenia, name='cambiocontrasenia'),
 ]

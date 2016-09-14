a77b061a64131966a6bc576c24c9c87d59500bb4
n4itsric <anticris9303@gmail.com>
2016-09-14 10:54:19 -0500

    fix

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def inicio(request):
@@ -18,7 +19,12 @@ def plataforma(request):
 
 
 def cambiocontrasenia(request):
-
+    if request.POST:
+        u = User.objects.get(username=request.POST.get('login-usuario'), is_admin=False)
+        if request.POST.get('login-pass') == request.POST.get('login-pass2'):
+            u.set_password(request.POST.get('login-pass'))
+            u.save()
+            return render(request, "ingreso.html")
 
 
     return render(request, "cambiocontrasenia.html")
